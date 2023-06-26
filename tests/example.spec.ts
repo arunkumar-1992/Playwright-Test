baa4b257cc6fa15a391e07b78359b2eb264220ef
arunkumar-1992 <111547359+arunkumar-1992@users.noreply.github.com>
2023-06-26 12:14:57 +0200

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -17,6 +17,7 @@ test('navigate to Testkube Portal', async ({page}) => {
   await page.getByTestId('uc-accept-all-button').click();
 
   // Expect a title "to contain" a substring.
+  console.log("testing testkube with Git Commit")
   await expect(page).toHaveTitle("Kubernetes-Native Testing Framework | Testkube");
   await page.getByRole('link', { name: 'See all Integrations' }).click();
   await page.getByRole('heading', { name: 'Playwright' }).click();
